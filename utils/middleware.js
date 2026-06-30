af33ef3da6e9661c3eba1c5baf7d85b5aa9b0ca5
Jaakko Hietikko <jhietikk22@student.oulu.fi>
2026-06-30 12:02:47 +0300

    4.3 done + added eslint

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,7 +21,7 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).json({ error: error.message })
     }
 
-  next(error)
+    next(error)
 }
 
 module.exports = {
