281d7dfcb92ea1f503d015bc094cfd77d7b4a8a2
Jaakko Hietikko <jhietikk22@student.oulu.fi>
2026-07-01 13:37:44 +0300

    4.15 done

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
     {
@@ -51,19 +52,16 @@ const initialBlogs = [
     }
 ]
 
-const nonExistingId = async () => {
-    const blog = new Blog({ content: 'willremovethissoon' })
-    await blog.save()
-    await blog.deleteOne()
-
-    return blog._id.toString()
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(b => b.toJSON())
 }
 
-const notesInDb = async () => {
-    const blogs = await Blog.find({})
-    return blogs.map(note => note.toJSON())
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(u => u.toJSON())
 }
 
 module.exports = {
-    initialBlogs, nonExistingId, notesInDb
+    initialBlogs, usersInDb, blogsInDb
 }
