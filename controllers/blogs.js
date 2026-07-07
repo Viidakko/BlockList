2e4126852ce671fdbaf60473c997bec0fe641abf
Jaakko Hietikko <jhietikk22@student.oulu.fi>
2026-07-07 11:30:29 +0300

    4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -54,8 +54,27 @@ blogsRouter.get('/:id', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+        return response.status(400).json({ error: 'userId missing or not valid' })
+    }
     try {
-        await Blog.findByIdAndDelete(request.params.id)
+        const blog = await Blog.findById(request.params.id)
+
+        if (!blog) {
+            return response.status(204).end()
+        }
+
+        if (blog.user.toString() !== user.id.toString()) {
+            return response.status(401).json({ error: 'token invalid' })
+        }
+
+        await blog.deleteOne()
         response.status(204).end()
     }
     catch (exception) {
